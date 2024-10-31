3460d40c0318b82512a5d5f1cb0b3b44c04c9c2c
Naveenkumar R N <152777442+Naveenkumarrn01@users.noreply.github.com>
2024-10-31 14:13:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const form = document.querySelector("#searchForm");
+const resultsDiv = document.querySelector("#results");
+const loadingDiv = document.querySelector("#loading");
+
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  // Clear previous results and show loading indicator
+  resultsDiv.innerHTML = "";
+  loadingDiv.style.display = "block";
+
+  const searchTerm = form.elements.query.value;
+  const config = { params: { q: searchTerm } };
+  try {
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    displayResults(res.data);
+  } catch (error) {
+    resultsDiv.innerHTML = "<p>Something went wrong. Please try again.</p>";
+  } finally {
+    loadingDiv.style.display = "none";
+  }
+
+  form.elements.query.value = ""; // Clear input
+});
+
+const displayResults = (shows) => {
+  if (shows.length === 0) {
+    resultsDiv.innerHTML = "<p>No results found.</p>";
+  } else {
+    for (let result of shows) {
+      if (result.show.image) {
+        const img = document.createElement("img");
+        img.src = result.show.image.medium;
+        resultsDiv.append(img);
+      }
+    }
+  }
+};
